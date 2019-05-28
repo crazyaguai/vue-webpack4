3a3e3fd3364161a13e931618bdd64eed823e4a47
yangxiaochuan <xiaochuan.yang@vivo.com>
2019-05-28 19:27:03 +0800

    添加eslint配置

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: [
+    'plugin:vue/recommended'
+  ],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    mocha: true,
+    jest: true,
+    jasmine: true,
+  },
+  rules: {
+  }
+}
