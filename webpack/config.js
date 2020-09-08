31d10ba4f37032d053f9f0cf7f46d093282614ce
11102164 <11102164@vivo.com>
2020-09-08 20:51:51 +0800

    change

--- webpack/config.js
+++ webpack/config.js
@@ -4,58 +4,58 @@ const path = require('path')
 const userConfig = require('../userConfig')
 
 module.exports = {
-    dev: {
-        // 路径
-        assetsSubDirectory: 'static',
-        assetsPublicPath: '/',
-        // 代理
-        proxyTable: userConfig.proxyTable,
+  dev: {
+    // 路径
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    // 代理
+    proxyTable: userConfig.proxyTable,
 
-        // devServer选项
-        host: '0.0.0.0', //
-        port: userConfig.port || 5000, //
-        //执行npm start --usehttps
-        https: process.env.npm_config_usehttps ? true : false,
-        autoOpenBrowser: false, // 自动打开浏览器
-        errorOverlay: true,
-        notifyOnErrors: true,
-        poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
+    // devServer选项
+    host: '0.0.0.0', //
+    port: userConfig.port || 5000, //
+    //执行npm start --usehttps
+    https: process.env.npm_config_usehttps ? true : false,
+    autoOpenBrowser: false, // 自动打开浏览器
+    errorOverlay: true,
+    notifyOnErrors: true,
+    poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
 
-        /**
+    /**
          * Source Maps
          */
-        devtool: 'cheap-module-eval-source-map',
+    devtool: 'cheap-module-eval-source-map',
 
-        // If you have problems debugging vue-files in devtools,
-        // set this to false - it *may* help
-        // https://vue-loader.vuejs.org/en/options.html#cachebusting
-        cacheBusting: true,
+    // If you have problems debugging vue-files in devtools,
+    // set this to false - it *may* help
+    // https://vue-loader.vuejs.org/en/options.html#cachebusting
+    cacheBusting: true,
 
-        cssSourceMap: true
-    },
+    cssSourceMap: true
+  },
 
-    build: {
-        // 路径
-        assetsRoot: path.resolve(__dirname, '../dist'),
-        assetsSubDirectory: 'static',
-        assetsPublicPath: '/',
+  build: {
+    // 路径
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
 
-        // 页面入口文件下，会自动检索下边所有html和js入口
-        pageEntry: path.resolve(__dirname, '../src/pages'),
+    // 页面入口文件下，会自动检索下边所有html和js入口
+    pageEntry: path.resolve(__dirname, '../src/pages'),
 
 
-        minChunks: 2, // 公共包的最小引用数目
+    minChunks: 2, // 公共包的最小引用数目
 
-        productionSourceMap: false,
-        // https://webpack.js.org/configuration/devtool/#production
-        devtool: '#source-map',
+    productionSourceMap: false,
+    // https://webpack.js.org/configuration/devtool/#production
+    devtool: '#source-map',
 
-        base64Limit: 8192, // 超过这个数字，则转为base64资源
+    base64Limit: 8192, // 超过这个数字，则转为base64资源
 
-        // 模块分析，通过npm run build --report可以打开
-        bundleAnalyzerReport: process.env.npm_config_report
-    },
-    // 外部引入的插件，在这里配置
-    externals: {},
+    // 模块分析，通过npm run build --report可以打开
+    bundleAnalyzerReport: process.env.npm_config_report
+  },
+  // 外部引入的插件，在这里配置
+  externals: {},
 }
--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -15,82 +15,82 @@ const HOST = process.env.HOST || config.dev.host
 const PORT = process.env.PORT && Number(process.env.PORT) || config.dev.port
 
 const devWebpackConfig = merge(baseWebpackConfig, {
-    mode: 'development',
-    module: {
-        rules: utils.styleLoaders({
-            sourceMap: config.dev.cssSourceMap,
-            usePostCSS: true,
-            extract: false
-        })
-    },
-    devtool: config.dev.devtool,
-    // devServer 配置，大部分在config中
-    devServer: {
-        clientLogLevel: 'warning',
+  mode: 'development',
+  module: {
+    rules: utils.styleLoaders({
+      sourceMap: config.dev.cssSourceMap,
+      usePostCSS: true,
+      extract: false
+    })
+  },
+  devtool: config.dev.devtool,
+  // devServer 配置，大部分在config中
+  devServer: {
+    clientLogLevel: 'warning',
 
-        hot: true,
-        contentBase: false, // 使用CopyWebpackPlugin, 所以这里不需要
-        compress: true,
-        host: HOST,
-        port: PORT,
-        https: config.dev.https,
-        open: config.dev.autoOpenBrowser,
-        overlay: config.dev.errorOverlay ?
-            {
-                warnings: false,
-                errors: true
-            } :
-            false, // 是否显示编译错误
-        publicPath: config.dev.assetsPublicPath,
-        proxy: config.dev.proxyTable,
-        quiet: true, // 使用FriendlyErrorsPlugin报错，所以把原生的报错关了
-        watchOptions: {
-            poll: config.dev.poll,
-        }
-    },
-    optimization: {
-        noEmitOnErrors: true,
-        namedModules: true,//热更新显示文件名
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: '"dev"',
-            },
-        }),
+    hot: true,
+    contentBase: false, // 使用CopyWebpackPlugin, 所以这里不需要
+    compress: true,
+    host: HOST,
+    port: PORT,
+    https: config.dev.https,
+    open: config.dev.autoOpenBrowser,
+    overlay: config.dev.errorOverlay ?
+      {
+        warnings: false,
+        errors: true
+      } :
+      false, // 是否显示编译错误
+    publicPath: config.dev.assetsPublicPath,
+    proxy: config.dev.proxyTable,
+    quiet: true, // 使用FriendlyErrorsPlugin报错，所以把原生的报错关了
+    watchOptions: {
+      poll: config.dev.poll,
+    }
+  },
+  optimization: {
+    noEmitOnErrors: true,
+    namedModules: true,//热更新显示文件名
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"dev"',
+      },
+    }),
 
-        new webpack.DllReferencePlugin({
-            context: path.resolve(__dirname, '../'), // 根路径
-            manifest: require('./dll/vueVendor-manifest.json'),
-        }),
+    new webpack.DllReferencePlugin({
+      context: path.resolve(__dirname, '../'), // 根路径
+      manifest: require('./dll/vueVendor-manifest.json'),
+    }),
 
-        new webpack.HotModuleReplacementPlugin(),
-    ]
+    new webpack.HotModuleReplacementPlugin(),
+  ]
 })
 
 // 前面一串是为了找在port被占用的情况下，自动找一个没被占用的
 module.exports = new Promise((resolve, reject) => {
-    portfinder.basePort = devWebpackConfig.devServer.port
-    portfinder.getPort((err, port) => {
-        if (err) {
-            reject(err)
-        } else {
-            // publish the new Port, necessary for e2e tests
-            process.env.PORT = port
-            // add port to devServer config
-            devWebpackConfig.devServer.port = port
-            let httpType = config.dev.https ? 'https':'http'
-            // Add FriendlyErrorsPlugin
-            devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
-                compilationSuccessInfo: {
-                    messages: [`开发地址: ${httpType}://${devWebpackConfig.devServer.host}:${port}  ${httpType}://127.0.0.1:${port}  ${httpType}://${ip.address()}:${port}`],
-                },
-                onErrors: config.dev.notifyOnErrors ?
-                    utils.createNotifierCallback() :
-                    undefined
-            }))
+  portfinder.basePort = devWebpackConfig.devServer.port
+  portfinder.getPort((err, port) => {
+    if (err) {
+      reject(err)
+    } else {
+      // publish the new Port, necessary for e2e tests
+      process.env.PORT = port
+      // add port to devServer config
+      devWebpackConfig.devServer.port = port
+      let httpType = config.dev.https ? 'https':'http'
+      // Add FriendlyErrorsPlugin
+      devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
+        compilationSuccessInfo: {
+          messages: [`开发地址: ${httpType}://${devWebpackConfig.devServer.host}:${port}  ${httpType}://127.0.0.1:${port}  ${httpType}://${ip.address()}:${port}`],
+        },
+        onErrors: config.dev.notifyOnErrors ?
+          utils.createNotifierCallback() :
+          undefined
+      }))
 
-            resolve(devWebpackConfig)
-        }
-    })
+      resolve(devWebpackConfig)
+    }
+  })
 })
